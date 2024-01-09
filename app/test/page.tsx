01a07bfcd8ebc824f09240e9b75122354275da23
Alex Straight <compsciaj@outlook.com>
2024-01-09 03:22:07 -0800

    adjusted theme toggle, deleted test page

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import MathToolbar from "@/components/math/math-tool-bar";
-
-export default function TestPage() {
-  return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <MathToolbar />
-    </div>
-  );
-}
